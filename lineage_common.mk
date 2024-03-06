d747f971ea43801009617cfbee48b69e6e0eb904
Matt Filetto <matt.filetto@gmail.com>
2024-03-06 15:56:54 -0800

    zuma: add service to disable PixelDisplayService during boot process.
    * We can't out-right delete the package because Pixel Thermometer for some reason relies on its presence for sensor permissions. So we just uninstall it for the user but keep the data so it can be used by Thermometer.
    
    With this, we can switch to EvoX's PixelParts for HBM, and enjoy the extra battery savings.
    
    Change-Id: I683cb36b64e91d5deeaf3f700c09cda86c3b1c8b
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- lineage_common.mk
+++ lineage_common.mk
@@ -45,5 +45,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     TetheringOverlay
 
+PRODUCT_COPY_FILES += \
+    device/google/zuma/conf/disable_pds.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/disable_pds.sh
+
 # Touch
 include hardware/google/pixel/touch/device.mk
