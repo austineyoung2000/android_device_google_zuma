a75468b8889ed4af3d073df909ab9d9905d66e33
Jrcable1 <jrcable1@gmail.com>
2024-11-24 16:07:30 -0500

    zuma: enable VoLTE, VoWIFI, VoNR call support & 5G for non-supported countries

--- device-common.mk
+++ device-common.mk
@@ -50,6 +50,8 @@ PRODUCT_MAX_PAGE_SIZE_SUPPORTED := 4096
 # Trigger fsck on upgrade (305658663)
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.preventative_fsck = 1
+    
+TARGET_SYSTEM_PROP := device/google/zuma/system.prop
 
 # Indicate that the bootloader supports the MTE developer option switch
 # (MISC_MEMTAG_MODE_MEMTAG_ONCE), with the exception of _fullmte products that
