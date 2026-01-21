0aaf44f5d85ade2b72ec6f88ee01a5a650bb7e49
Ionut Gherman <ghermanionut96@gmail.com>
2026-01-20 19:24:20 -0500

    zuma: add Google Camera if exists
    Change-Id: I94c1dd19032d49b345a2e6fc8bf6da1755cdf9fa
    Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

--- device.mk
+++ device.mk
@@ -669,3 +669,6 @@ DEVICE_MATRIX_FILE += \
     device/google/zuma/vintf/compatibility_matrix.xml
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += \
     device/google/zuma/vintf/device_framework_matrix_product.xml
+
+# Google Camera
+$(call inherit-product-if-exists, vendor/google/camera/camera.mk)
